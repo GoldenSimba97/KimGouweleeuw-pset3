f3f3929568636a140cfe5d13156bc441a30de223
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-19 16:34:00 +0200

    Alles uit Asynctask filmpjes gedaan

---
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset3/HttpRequestHelper.java
@@ -0,0 +1,43 @@
+package com.example.kimgo.kimgouweleeuw_pset3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by kimgo on 19-9-2017.
+ */
+
+public class HttpRequestHelper {
+
+    protected static synchronized String downloadFromServer(String... params) {
+        String result = "";
+        String chosenTag = params[0];
+
+        URL url = null;
+
+        HttpURLConnection connect;
+
+        if (url != null) {
+            try {
+                connect = (HttpURLConnection) url.openConnection();
+                connect.setRequestMethod("GET");
+
+                Integer responseCode = connect.getResponseCode();
+                if (responseCode >= 200 && responseCode < 300) {
+                    BufferedReader bufferedReader = new BufferedReader
+                            (new InputStreamReader(connect.getInputStream()));
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line;
+                    }
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset3/MainActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset3/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.kimgo.kimgouweleeuw_pset3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTrack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTrack = (EditText) findViewById(R.id.editTrack);
+        assert editTrack != null;
+    }
+
+    public void trackSearch(View view) {
+        String trackSearch = editTrack.getText().toString();
+        TrackAsyncTask asyncTask = new TrackAsyncTask(this);
+        asyncTask.execute(trackSearch);
+
+        editTrack.getText().clear();
+    }
+
+    public void trackStartIntent(ArrayList<String> trackData) {
+        Intent dataIntent = new Intent(this, DataActivity.class);
+        dataIntent.putExtra("data", trackData);
+        this.startActivity(dataIntent);
     }
 }
